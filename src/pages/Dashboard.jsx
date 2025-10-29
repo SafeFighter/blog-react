81561b06e75cbbc0ba731d0e3427f9ffb1524ff8
Matija Miletić <matoma002@gmail.com>
2025-10-29 22:30:21 +0100

    Dashboard - created admin view

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { supabase } from "../supabase";
 
 function Dashboard() {
+  const [user, setUser] = useState(null);
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  useEffect(() => {
+    async function fetchUser() {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      setUser(user);
+      if (user) {
+        const { data: adminRows } = await supabase
+          .from("admins")
+          .select("user_id")
+          .eq("user_id", user.id);
+        setIsAdmin(adminRows.length > 0);
+      }
+    }
+    fetchUser();
+  }, []);
+
   return (
     <>
       <h1>Dashboard menu! Welcome!</h1>
-      <Link to="/blog/create">
-        <button>Create new post</button>
-      </Link>
+      {isAdmin ? (
+        <Link to="/blog/create">
+          <button>Create new post</button>
+        </Link>
+      ) : (
+        "You are not admin"
+      )}
     </>
   );
 }
