ff27c85aa0c302f9022b6d1d1b7a7f7f02fdf954
Matija Miletić <matoma002@gmail.com>
2025-12-07 16:48:44 +0100

    SearchPost function completed

--- src/pages/SearchPost.jsx
+++ src/pages/SearchPost.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react";
+import { supabase } from "../supabase";
+import PostCard from "../components/PostCard";
+
 function SearchPost() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [result, setResults] = useState([]);
+
+  const handleNewSearch = async (e) => {
+    e.preventDefault();
+
+    if (!searchQuery) return;
+
+    const { data, error } = await supabase
+      .from("posts")
+      .select("*")
+      .ilike("title", `%${searchQuery}%`);
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    setResults(data);
+  };
+
   return (
     <>
       <div className="container">
@@ -6,11 +31,23 @@ function SearchPost() {
           <h3>Search Posts</h3>
         </div>
         <div>
-          <form>
-            <input type="text" placeholder="Input your search words here" />
+          <form onSubmit={handleNewSearch}>
+            <input
+              size="35"
+              type="text"
+              placeholder="Input your search words here"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
             <button type="submit">🔍 Search</button>
           </form>
         </div>
+      </div>
+
+      <div className="result">
+        {result.map((post) => (
+          <PostCard key={post.id} post={post} />
+        ))}
       </div>
     </>
   );
